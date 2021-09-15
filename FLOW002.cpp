9a1d4803d30512f28085467a068bd8c089dcd1da
chirantan-acharyya <acharyya.chirantan@gmail.com>
2021-09-15 09:05:37 +0530

    Added FLOW002 and START01

---
+++ FLOW002.cpp
@@ -0,0 +1,25 @@
+/*
+     Problem Statement is given here:-
+     https://www.codechef.com/problems/FLOW016/
+*/
+#include <bits/stdc++.h> 
+
+using namespace std;
+
+int main() {
+	// Read the number of test cases.
+	int T;
+	scanf("%d", &T);
+	while (T--) {
+		// Read the input a, b
+		int a, b;
+		scanf("%d %d", &a, &b);
+
+		// Compute the ans.
+		// Complete the below line.
+		int ans =a%b;
+		printf("%d\n", ans);
+	}
+
+	return 0;
+}
